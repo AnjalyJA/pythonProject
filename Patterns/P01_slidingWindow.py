12903f8be94cb6f611d087d2670ab39fc8711749
AnjalyJA <anjalyxclusive@gmail.com>
2022-08-31 16:56:07 -0500

    2nd pattern done

--- Patterns/P01_slidingWindow.py
+++ Patterns/P01_slidingWindow.py
@@ -16,17 +16,18 @@ def avgOfSubarray2(arr,k):
     result = []
     windowStart = 0
     windowSum = 0
+
     for windowEnd in range(0,len(arr)):
         windowSum += arr[windowEnd]
-
         if(windowEnd>=k-1):
             result.append(windowSum/k)
             windowSum -= arr[windowStart]
             windowStart += 1
+
     return result
 
 
-def maxSubarraySizeK():
+def maxSubarraySizeK(arr,k):
     print("#2: maxSubarraySizeK")
     maxSum = 0
     windowSum = 0
@@ -34,7 +35,6 @@ def maxSubarraySizeK():
 
     for windowEnd in range(0,len(arr)):
         windowSum += arr[windowEnd]
-
         if(windowEnd>=k-1):
             maxSum = max(windowSum,maxSum)
             windowSum -= arr[windowStart]
@@ -42,14 +42,25 @@ def maxSubarraySizeK():
 
     return maxSum
 
+def smallestSubarrayWithGivenSum(arr,k):
+    result=[]
+
+
+
 
 # 1: Find Avg of sub-array
 # https://leetcode.com/problems/maximum-average-subarray-i/
+# TimeComp : O(N)
 arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 k = 5
 print(avgOfSubarray2(arr,k))
 
 # 2: Max subarray of size k
 # https://leetcode.com/problems/largest-subarray-length-k/
-print(maxSubarraySizeK())
+# TimeComp : O(N)  Space : O(1)
+print(maxSubarraySizeK(arr,k))
 
+# 3: Smallest Subarray With Given Sum
+# https://leetcode.com/problems/minimum-size-subarray-sum/
+# T :  S :
+#print(smallestSubarrayWithGivenSum(arr,k))
