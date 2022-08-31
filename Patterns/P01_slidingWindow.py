13a05ff0d44b2f3f4a57904b1fd721988523db79
AnjalyJA <anjalyxclusive@gmail.com>
2022-08-31 16:30:41 -0500

    2nd pattern done

--- Patterns/P01_slidingWindow.py
+++ Patterns/P01_slidingWindow.py
@@ -1,5 +1,5 @@
 
-def avgSubArray(arr,k):
+def avgSubArray1(arr,k):
     print("#1: avgSubArray")
     result=[]
 
@@ -11,17 +11,45 @@ def avgSubArray(arr,k):
 
     return result
 
+def avgOfSubarray2(arr,k):
+    print("#1: avgSubArray")
+    result = []
+    windowStart = 0
+    windowSum = 0
+    for windowEnd in range(0,len(arr)):
+        windowSum += arr[windowEnd]
+
+        if(windowEnd>=k-1):
+            result.append(windowSum/k)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+    return result
+
 
 def maxSubarraySizeK():
     print("#2: maxSubarraySizeK")
+    maxSum = 0
+    windowSum = 0
+    windowStart = 0
+
+    for windowEnd in range(0,len(arr)):
+        windowSum += arr[windowEnd]
+
+        if(windowEnd>=k-1):
+            maxSum = max(windowSum,maxSum)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+
+    return maxSum
+
 
 # 1: Find Avg of sub-array
 # https://leetcode.com/problems/maximum-average-subarray-i/
 arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 k = 5
-print(avgSubArray(arr,k))
+print(avgOfSubarray2(arr,k))
 
 # 2: Max subarray of size k
 # https://leetcode.com/problems/largest-subarray-length-k/
-maxSubarraySizeK()
+print(maxSubarraySizeK())
 
