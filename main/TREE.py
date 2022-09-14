b7837467a88cfcae672fe63d49d45cdb80697550
AnjalyJA <anjalyxclusive@gmail.com>
2022-09-14 16:16:24 -0500

    updated

--- main/TREE.py
+++ main/TREE.py
@@ -1,86 +1,99 @@
 #BINARY SEARCH TREE
 
 class Node:
-    def __init__(self, data):
+
+    def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
 
-    def insertNode(self, data):
+    def insert(self,data):
         if self.data:
-            if data < self.data:
+            if self.data > data:
                 if self.left is None:
-                    self.left = Node(data)
+                    self.left=Node(data)
                 else:
-                    self.left.insertNode(data)
-            elif data > self.data:
+                    self.left.insert(data)
+            elif self.data < data:
                 if self.right is None:
-                    self.right = Node(data)
+                    self.right=Node(data)
                 else:
-                    self.right.insertNode(data)
+                    self.right.insert(data)
         else:
-            print("else part")
             self.data = data
 
-    def printTree(self):
-        if self.left:
-            self.left.printTree()
-        print(self.data)
-        if self.right:
-            self.right.printTree()
-
-    #stack
     def inOrderTraversal(self,root):
-        current = root
+        print("inOrderTraversal")
         stack = []
+        current = root
         while True:
             if current is not None:
                 stack.append(current)
                 current = current.left
-            elif(stack):
+            elif stack:
                 current = stack.pop()
-                print(current.data,end = " ")
+                print(current.data, end=" ")
                 current = current.right
             else:
                 break
+        print("\n")
 
-    #recursion
-    def preorderTraversal1(self, root):
-        res = []
-        if root:
-            res.append(root.data)
-            res = res + self.preorderTraversal1(root.left)
-            res = res + self.preorderTraversal1(root.right)
-        return res
-
-    #stack
-    def preOrderTraversal2(self,root):
-        nodeStack = []
-        nodeStack.append(root)
-
-        while(len(nodeStack)>0):
-            node = nodeStack.pop()
-            print(node.data, end = " ")
+    def preOrderTraversal(self,root):
+        print("preOrderTraversal")
+        stack = []
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            print(node.data, end=" ")
             if node.right is not None:
-                nodeStack.append(node.right)
+                stack.append(node.right)
             if node.left is not None:
-                nodeStack.append(node.left)
+                stack.append(node.left)
+        print("\n")
 
 
+    def postOrderTraversal(self,root):
+        stack = []
+        print("postOrderTraversal")
+        while True:
+            while root:
+                if root.right is not None:
+                    stack.append(root.right)
+                stack.append(root)
+                root = root.left
+
+            root = stack.pop()
+            if len(stack)>0:
+                if root.right is not None and stack[-1]==root.right:
+                    stack.pop()
+                    stack.append(root)
+                    root = root.right
+                else:
+                    print(root.data,end=" ")
+                    root = None
+            else:
+                break
+        print("\n")
 
 
+    def printTree(self):
 
+        if self.left is not None:
+            self.left.printTree()
+        print(self.data, end = " ")
+        if self.right is not None:
+            self.right.printTree()
 
+root = Node(5)
+root.insert(3)
+root.insert(2)
+root.insert(4)
+root.insert(7)
+root.insert(6)
+root.insert(8)
+root.insert(1)
 
-
-
-root = Node(12)
-root.insertNode(6)
-root.insertNode(14)
-root.insertNode(3)
-root.insertNode(4)
-root.insertNode(31)
-
+root.inOrderTraversal(root)
+root.preOrderTraversal(root)
+root.postOrderTraversal(root)
 root.printTree()
-root.preOrderTraversal2(root)
-
