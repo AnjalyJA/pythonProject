2ee81b160248adefb05ccc6d5a036857a53d3af0
AnjalyJA <anjalyxclusive@gmail.com>
2022-09-15 17:13:36 -0500

    1 module done

---
+++ Patterns/P07_TreeBFS.py
@@ -0,0 +1,51 @@
+#1. Binary Tree Level Order Traversal (easy)
+#https://leetcode.com/problems/binary-tree-level-order-traversal/
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+    def insert(self,data):
+        if self.data:
+            if self.data > data:
+                if self.left is None:
+                    self.left=Node(data)
+                else:
+                    self.left.insert(data)
+            elif self.data < data:
+                if self.right is None:
+                    self.right=Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def levelOrder(self, root):
+        if root is None:
+            return root
+        queue = []
+        return_list = []
+        queue.append(root)
+        while len(queue) > 0:
+            ans = []
+            l = len(queue)
+            for x in range(l):
+                node = queue.pop(0)
+                ans.append(node.data)
+                if node.left != None:
+                    queue.append(node.left)
+                if node.right != None:
+                    queue.append(node.right)
+            return_list.append(ans)
+        return return_list
+
+root = Node(5)
+root.insert(3)
+root.insert(2)
+root.insert(4)
+root.insert(7)
+root.insert(6)
+root.insert(8)
+root.insert(1)
+
+print(root.levelOrder(root))
