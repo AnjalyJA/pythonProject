4e47f1846b7aa2df8a112cf4650f1bf883f1a4af
AnjalyJA <anjalyxclusive@gmail.com>
2022-09-22 21:24:43 -0500

    2 function done

--- main/LeetCodeLevel1Badge.py
+++ main/LeetCodeLevel1Badge.py
@@ -1,11 +1,22 @@
-#https://leetcode.com/problems/running-sum-of-1d-array/
-#1480.Running Sum of 1d Array
+from numpy import inf
+def runningSum(nums):
+    runningSum = []
+    ele = 0
+    for i in nums:
+        ele += i
+        runningSum.append(ele)
+    return runningSum
 
-#https://leetcode.com/problems/find-pivot-index/
-#724.Find Pivot Index
+def pivotIndex(nums):
+    start = 0
+    end = sum(nums)
+    for index, num in enumerate(nums):
+        end -= num
+        if end == start:
+            return index
+        start += num
+    return -1
 
-#https://leetcode.com/problems/isomorphic-strings/
-#3.Isomorphic Strings
 def isIsomorphic(s,t):
     mapping_s_t = {}
     mapping_t_s = {}
@@ -17,8 +28,7 @@ def isIsomorphic(s,t):
             return False
     return True
 
-#https://leetcode.com/problems/is-subsequence/
-#4. Is Subsequence
+
 def isSubsequence(s,t):
     i,j = 0,0
     while i<len(s) and j<len(t):
@@ -28,8 +38,6 @@ def isSubsequence(s,t):
     return True if i == len(s) else False
 
 
-#https://leetcode.com/problems/number-of-matching-subsequences/
-#4.1 Number of Matching Subsequences
 def numMatchingSubseq(s,words):
     count = 0
     def isSub(s,t):
@@ -57,19 +65,62 @@ def numMatchingSubseq(s,words):
 
     return count
 
-#https://leetcode.com/problems/merge-two-sorted-lists/
-#6.Merge Two Sorted Lists
+def maxProfit(prices):
+    minPrice = float(inf)
+    maxProfit = 0
+    for i in range(len(prices)):
+        if prices[i] < minPrice:
+            minPrice = prices[i]
+        elif prices[i] - minPrice > maxProfit:
+            maxProfit = prices[i] - minPrice
+    return maxProfit
 
-'''if isIsomorphic("egg","bddg"):
+
+#https://leetcode.com/problems/running-sum-of-1d-array/
+#1480.Running Sum of 1d Array
+nums = [1,2,3,4]
+print("1.running sum")
+print(runningSum(nums))
+
+#https://leetcode.com/problems/find-pivot-index/
+#724.Find Pivot Index
+print("2.pivot index")
+print(pivotIndex([1,7,3,6,5,6]))
+
+#https://leetcode.com/problems/isomorphic-strings/
+#3.Isomorphic Strings
+print("3.isomorphic string?")
+if isIsomorphic("egg","bddg"):
     print("yes")
 else:
     print("no")
 
+#https://leetcode.com/problems/is-subsequence/
+#4. Is Subsequence
+print("4.subsequence?")
 if isSubsequence("aec","abcde"):
     print("yes")
 else:
-    print("no")'''
+    print("no")
 
+#https://leetcode.com/problems/number-of-matching-subsequences/
+#4.1 Number of Matching Subsequences
+print("4.1.Number of Matching Subsequences")
 s = "abcde"
 words = ["a","bb","acd","ace"]
 print(numMatchingSubseq(s,words))
+
+#https://leetcode.com/problems/merge-two-sorted-lists/
+#6.Merge Two Sorted Lists
+
+#https://leetcode.com/problems/middle-of-the-linked-list/
+#7.Middle of the Linked List
+
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+#9.Best Time to Buy and Sell Stock
+print("9.Best Time to Buy and Sell Stock")
+prices = [7,1,5,3,6,4]
+print(maxProfit(prices))
+
+#https://leetcode.com/problems/longest-palindrome/
+#10.Longest Palindrome
